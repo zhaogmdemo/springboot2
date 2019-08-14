c4c7be7b48185e04cb387239e669e57b3b177a7d
ZL0101 <31301959+ZL0101@users.noreply.github.com>
2019-08-14 23:31:29 +0800

    zl  2

---
+++ src/main/java/com/example/SpringBootVideo/SpringBootVideoApplication.java
@@ -0,0 +1,16 @@
+package com.example.SpringBootVideo;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.example.SpringBootVideo.mapper")
+public class SpringBootVideoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("here111");
+		SpringApplication.run(SpringBootVideoApplication.class, args);
+	}
+
+}
